df567bc75b7b8eae8b4e310e95c683bc399f1e35
RakaAF123 <rakaadmiharfan@gmail.com>
2024-04-08 02:05:59 +0700

    update aos gallery

--- src/components/img-box.tsx
+++ src/components/img-box.tsx
@@ -1,6 +1,6 @@
 export default function ImageBox({ url }: { url: string }) {
   return (
-    <div className="border border-gray-200 rounded-xl overflow-hidden justify-evenly">
+    <div className="rounded-xl overflow-hidden justify-evenly">
       <img src={url} alt="Image" className="h-auto w-full" />
     </div>
   );
