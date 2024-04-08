0678df815e73dd3be077fd9432a3c2d777ffefb8
RakaAF123 <rakaadmiharfan@gmail.com>
2024-04-08 16:16:16 +0700

    responsif for gallery dan membership page

--- src/app/(site)/gallery/page.tsx
+++ src/app/(site)/gallery/page.tsx
@@ -11,34 +11,49 @@ export default function gallery() {
   });
 
   return (
-    <div className="flex flex-col bg-white  relative h h-full w-full pt-48 pb-12 px-6">
+    <div className="flex flex-col bg-white relative h h-full w-full pt-32 md:pt-40 lg:pt-48 pb-12 px-6 overflow-hidden">
       <h1
-        className="text-6xl text-[#1F3576] font-bold text-poppins mx-auto"
+        className="text-3xl md:text-6xl text-[#1F3576] font-bold text-poppins mx-auto"
         data-aos="fade-down"
       >
         SPE UI GALLERY
       </h1>
-      <div className="flex flex-row gap-4 mt-12" data-aos="fade-right">
+      <div
+        className="flex flex-col md:flex-row gap-4 mt-12"
+        data-aos="fade-right"
+      >
         <ImageBox url={"./gallery/rectangle 803-min.png"} />
         <ImageBox url={"./gallery/rectangle 804-min.png"} />
         <ImageBox url={"./gallery/rectangle 805-min.png"} />
       </div>
-      <div className="flex flex-row gap-4 mt-12" data-aos="fade-left">
+      <div
+        className="flex flex-col md:flex-row gap-4 mt-12"
+        data-aos="fade-left"
+      >
         <ImageBox url={"./gallery/rectangle 806-min.png"} />
         <ImageBox url={"./gallery/rectangle 807-min.png"} />
         <ImageBox url={"./gallery/rectangle 808-min.png"} />
       </div>
-      <div className="flex flex-row gap-4 mt-12" data-aos="fade-right">
+      <div
+        className="flex flex-col md:flex-row gap-4 mt-12"
+        data-aos="fade-right"
+      >
         <ImageBox url={"./gallery/rectangle 809-min.png"} />
         <ImageBox url={"./gallery/rectangle 810-min.png"} />
         <ImageBox url={"./gallery/IMG_8909 1-min.png"} />
       </div>
-      <div className="flex flex-row gap-4 mt-12" data-aos="fade-left">
+      <div
+        className="flex flex-col md:flex-row gap-4 mt-12"
+        data-aos="fade-left"
+      >
         <ImageBox url={"./gallery/Screenshot (550) 1-min.png"} />
         <ImageBox url={"./gallery/image 134-min.png"} />
         <ImageBox url={"./gallery/image 135-min.png"} />
       </div>
-      <div className="flex flex-row gap-4 mt-12" data-aos="fade-right">
+      <div
+        className="flex flex-col md:flex-row gap-4 mt-12"
+        data-aos="fade-right"
+      >
         <ImageBox url={"./gallery/image 136-min.png"} />
         <ImageBox url={"./gallery/IMG_4279 1-min.png"} />
       </div>
--- src/app/(site)/membership/page.tsx
+++ src/app/(site)/membership/page.tsx
@@ -4,17 +4,16 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 export default function Membership() {
   return (
     <>
-      <div className="w-full h-auto lg:min-h-[874px] bg-gradient-to-b from-black to-[#D9D9D9] relative overflow-hidden">
+      <div className="w-full h-auto lg:min-h-[874px] bg-white relative overflow-hidden">
         <img
           src="/assets/image copy.png"
-          alt="Car Picture"
-          className="absolute my-auto top-[80px] lg:top-[120px] bottom-0 w-[70%] md:w-[55.83%] xl:w-full"
+          className="absolute my-auto top-[80px] lg:top-[120px] bottom-0 h-full w-full"
         />
-        <div className="bg-white bg-opacity-35 backdrop-blur-md w-[60%] md:w-1/2 h-[126vh] pt-[120px] lg:pt-[208px] pb-[96px] lg:pb-[180px] px-[7.5%] lg:px-[92px] border-[1px] border-white">
+        <div className="bg-white bg-opacity-35 backdrop-blur-md w-[60%] md:w-1/2 h-[126vh] pt-[110px] lg:pt-[208px] pb-[96px] lg:pb-[180px] px-[7.5%] lg:px-[92px] border-[1px] border-white">
           <h1 className="text-black font-bold text-[24px] md:text-[40px] lg:text-[56px]">
             SPE UI SC 2024
           </h1>
-          <p className="text-black font-poppins text-[14px] md:text-[16px] lg:text-[20px] md:text-justify mt-6 lg:mt-8">
+          <p className="text-black font-poppins text-[12px] md:text-[16px] lg:text-[20px] md:text-justify mt-6 lg:mt-8">
             SPE as the{" "}
             <span className="font-bold">
               largest and internationally recognized oil and gas organization
@@ -22,25 +21,25 @@ export default function Membership() {
             , provides students with technical oil and gas knowledge and soft
             skills needed to solve the impending energy challenges
           </p>
-          <p className="text-black font-poppins text-[14px] md:text-[16px] lg:text-[20px] md:text-justify mt-6 lg:mt-8">
+          <p className="text-black font-poppins text-[12px] md:text-[16px] lg:text-[20px] md:text-justify mt-6 lg:mt-8">
             Becoming an SPE member will provide you with many opportunities to{" "}
             <span className="font-bold">network with other </span> student
             members and local/global industry professionals and{" "}
             <span className="font-bold">acquire career-advancing skills </span>{" "}
             through our work programs.
           </p>
-          <div className="flex mt-5 lg:mt-10 gap-8">
+          <div className="flex flex-col lg:flex-row mt-10 gap-2 md:gap-4">
             <Link
               href={"https://bit.ly/JoinSPE2024"}
-              className="flex flex-row w-fit justify-center items-center gap-4 bg-sky-950 px-6 py-2 rounded-full text-[14px] md:text-[16px] lg:text-[24px] text-white font-semibold"
+              className="flex flex-row w-fit justify-center items-center gap-4 bg-sky-950 px-4 py-2 md:px-6 md:py-2 rounded-full text-[14px] md:text-[16px] lg:text-[20px] text-white font-semibold"
             >
-              Join Us
+              Register
               <FaExternalLinkAlt />
             </Link>
 
             <Link
               href={"https://bit.ly/SPEUISC2024MembershipGuidebook"}
-              className="flex flex-row w-fit justify-center items-center gap-4 bg-sky-950 px-6 py-2 rounded-full text-[14px] md:text-[16px] lg:text-[24px] text-white font-semibold"
+              className="flex flex-row w-fit justify-center items-center gap-4 bg-sky-950 px-4 py-2 md:px-6 md:py-2 rounded-full text-[14px] md:text-[16px] lg:text-[20px] text-white font-semibold"
             >
               Guidebook
               <FaExternalLinkAlt />
@@ -50,15 +49,10 @@ export default function Membership() {
       </div>
 
       <div className="w-full bg-white text-dark-maintext pt-[36px] lg:pt-[88px] pb-[44px] lg:pb-[100px] px-[6.805%] lg:px-[98px] relative overflow-hidden">
-        <img
-          src="/assets/car_2.svg"
-          alt="Car Picture"
-          className="hidden md:block absolute top-0 bottom-0 my-auto w-[75%] md:w-[50.55%] xl:w-auto -right-[11.11%]"
-        />
-        <h1 className="font-robotoSlab font-bold text-[32px] md:text-[40px] lg:text-[56px]">
+        <h1 className="font-bold text-[32px] md:text-[40px] lg:text-[56px]">
           What Will You Get as Our Member?
         </h1>
-        <div className="w-full md:w-[60%] max-w-[766px]">
+        <div className="w-4/5">
           <div className="flex flex-col gap-9 mt-8 lg:mt-10">
             <div className="flex gap-6">
               <div className="flex items-center px-5 lg:px-10 border-r-8 border-[#FCCF37] font-poppins font-semibold text-[24px] md:text-[36px] lg:text-[48px]">
