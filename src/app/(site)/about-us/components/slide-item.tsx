f5664bd47e0ff0c9dc01520a4169c1d235a7973b
akmalnatha <akmalabhirama2002@gmail.com>
2024-04-23 17:31:14 +0700

    style: about us

--- src/app/(site)/about-us/components/slide-item.tsx
+++ src/app/(site)/about-us/components/slide-item.tsx
@@ -11,7 +11,7 @@ function SlideItem({ className, type, active }: SlideItemProps) {
     <div className={clsx(className, "w-screen lg:w-[33vw] flex justify-center")}>
       <div
         className={clsx(
-          "relative w-80 h-fit bg-transparent flex justify-center items-center duration-200"
+          "relative w-60 md:w-72 lg:w-80 h-fit bg-transparent flex justify-center items-center duration-200"
         )}
       >
         {/* <div className={clsx("absolute w-80 h-80 bg-white/90 rounded-full duration-1000", active && "z-[-1]", !active && "z-[1]")}>
--- src/app/(site)/about-us/page.tsx
+++ src/app/(site)/about-us/page.tsx
@@ -2,34 +2,71 @@
 
 import MarqueeComponent from "@/components/marquee";
 import Marquee from "@/components/marquee";
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import {
+  AnimatePresence,
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import Slides from "./components/slide";
 import Counter from "./components/counter";
 
 export default function AboutUs() {
-  const containerRef = useRef(null);
+  const [title] = useState(["Resilient", "Escalate", "Comprehensive"]); // Define your title here
+  const [description] = useState([
+    "SPE is an international non-profit organization that provides unparalalled insights, shared expertise, and life-long learning to fuel the success of our members and the future of the oil and gas industry.",
+    "SPE is an international non-profit organization that provides unparalalled insights, shared expertise, and life-long learning to fuel the success of our members and the future of the oil and gas industry.",
+    "SPE is an international non-profit organization that provides unparalalled insights, shared expertise, and life-long learning to fuel the success of our members and the future of the oil and gas industry.",
+  ]); // Define your description here
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animationKey, setAnimationKey] = useState(0);
+  const [isMobile, setIsMobile] = useState<boolean>();
 
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["start end", "end end"],
-  });
+  useEffect(() => {
+    setCurrentIndex(0); // Reset currentIndex when the screen size changes
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % title.length);
+      setAnimationKey((prevKey) => (prevKey + 1) % title.length); // Update the key to trigger animation restart
+    }, 7000); // Change sentence every 5 seconds
+
+    return () => clearInterval(interval);
+  }, [isMobile]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window !== "undefined") {
+        setIsMobile(window.innerWidth < 1024);
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
+  }, []);
 
   return (
     <>
-      <motion.div 
-      variants={{
-        hidden: {
-          opacity: 1,
-        },
-        show: {
-          opacity: 1,
-        },
-      }}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0 }}
-      className="w-full h-fit relative overflow-hidden pt-[56px] lg:pt-[72px]">
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 1,
+          },
+          show: {
+            opacity: 1,
+          },
+        }}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="w-full h-fit relative overflow-hidden pt-[56px] lg:pt-[72px]"
+      >
         <div className="w-full flex flex-col absolute top-[32%] sm:top-[23%] lg:top-[20%]">
           <motion.div
             variants={{
@@ -48,7 +85,7 @@ export default function AboutUs() {
                 },
               },
             }}
-            className="text-center text-[24px] sm:text-[32px] md:text-[40px] lg:text-[52px] xl:text-[64px] text-[#FCCF37] drop-shadow-md font-leagueSpartan font-semibold relative"
+            className="text-center text-[24px] sm:text-[32px] md:text-[40px] lg:text-[52px] xl:text-[64px] text-[#FCCF37] font-leagueSpartan font-semibold relative"
           >
             <h1 className="leading-tight relative z-[2]">SPE UI SC</h1>
             <h1 className="leading-tight absolute text-center text-[#2F2F2F] top-[2px] left-[2px] opacity-35 w-full">
@@ -72,9 +109,11 @@ export default function AboutUs() {
                 },
               },
             }}
-            className="text-center text-[16px] md:text-[24px] lg:text-[36px] xl:text-[48px] text-[#2F2F2F] drop-shadow-md font-lato italic font-semibold relative"
+            className="text-center text-[20px] md:text-[28px] lg:text-[36px] xl:text-[48px] text-[#2F2F2F] font-lato italic font-semibold relative"
           >
-            <h1 className="leading-tight relative z-[2]">Strive Excellence, Solve the Challenge</h1>
+            <h1 className="leading-tight relative z-[2]">
+              Strive Excellence, Solve the Challenge
+            </h1>
             <h1 className="leading-tight absolute text-center text-[#2F2F2F] top-[2px] left-[2px] opacity-35 w-full">
               Strive Excellence, Solve the Challenge
             </h1>
@@ -91,9 +130,9 @@ export default function AboutUs() {
           className="object-contain absolute bottom-0 z-[2] w-full"
         />
         <div
-          className="h-[8%] w-full absolute bottom-0 z-[3]"
+          className="h-[16%] lg:h-[8%] w-full absolute bottom-0 z-[3]"
           style={{
-            boxShadow: "inset 0 -28px 24px -8px rgba(255, 255, 255, 1)",
+            boxShadow: "inset 0 -28px 16px -12px rgba(255, 255, 255, 1)",
           }}
         ></div>
       </motion.div>
@@ -103,10 +142,15 @@ export default function AboutUs() {
         <h1 className="px-5 text-yellow-400 font-lato italic">SPE UI SC - Outstand Beyond Ordinary</h1>
         <h1 className="px-5 text-yellow-400 font-lato italic">SPE UI SC - Outstand Beyond Ordinary</h1>
       </MarqueeComponent> */}
-      <div className="bg-[#FFFFFF] pt-[1.3%] pb-[2.5%]">
-        <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] shadow-black text-[#1F3576] font-poppins font-bold text-center mb-4">
-          Who We Are
-        </h2>
+      <div className="w-full bg-[#FFFFFF] pt-[1.3%] pb-[2.5%]">
+        <div className="w-full relative overflow-hidden mb-4">
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-[#1F3576] font-poppins font-bold text-center relative z-[2]">
+            Who <span className="text-[#FBD233]">We</span> Are
+          </h2>
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-[#2F2F2F] font-poppins font-bold text-center absolute top-[2px] left-[2px] opacity-35 w-full">
+            Who We Are
+          </h2>
+        </div>
         <p className="text-[14px] md:text-[16px] lg:text-[20px] text-[#1F3576] font-poppins text-justify px-[3.333%]">
           SPE is the{" "}
           <span className="font-bold">
@@ -163,14 +207,103 @@ export default function AboutUs() {
           </div>
         </div>
       </div>
-      <div className="bg-[#FFFFFF] pt-5 pb-10">
-        <h3 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-center text-[#1F3576] font-leagueSpartan font-semibold">
-          <span className=" text-yellow-300 font-lato italic font-medium">
-            Our
-          </span>{" "}
-          Core Values
-        </h3>
+      <div className="w-full bg-[#FFFFFF] pb-[2.5%]">
+        <div className="w-full relative overflow-hidden">
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-center text-[#1F3576] font-poppins font-bold leading-snug relative z-[2]">
+            <span className="text-[#FBD233]">Our</span> Core Values
+          </h2>
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-center text-[#2F2F2F] font-poppins font-bold leading-snug absolute top-[2px] left-[2px] opacity-35 w-full">
+            Our Core Values
+          </h2>
+        </div>
         <Slides />
+        <motion.div
+          key={animationKey}
+          variants={{
+            hidden: {
+              opacity: 1,
+            },
+            show: {
+              opacity: 1,
+            },
+          }}
+          className={`w-[90%] h-auto mx-auto rounded-xl bg-gradient-to-r flex flex-col justify-start gap-2 md:gap-5 lg:justify-between items-center p-[3%] md:p-[2%] lg:mt-7 ${
+            currentIndex == 1
+              ? "md:flex-row-reverse from-[#213D73] to-[#4B65B3]"
+              : "md:flex-row from-[#4B65B3] to-[#213D73]"
+          }`}
+        >
+          <AnimatePresence>
+            <motion.div
+              key={title[currentIndex]}
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  duration: 0.75,
+                  delay: 0,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.75,
+                  delay: 0,
+                  ease: "easeInOut",
+                },
+              }}
+              className="text-center text-[20px] md:text-[28px] lg:text-[36px] xl:text-[44px] text-[#FBD233] font-poppins font-bold relative"
+            >
+              <h3 className="relative z-[2]">
+                {title[currentIndex]}
+              </h3>
+              <h3 className="absolute text-center text-[#2F2F2F] top-[2px] left-[2px] opacity-35 w-full">
+                {title[currentIndex]}
+              </h3>
+            </motion.div>
+            <motion.div
+              key={description[currentIndex]}
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  duration: 0.75,
+                  delay: 0.1,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.75,
+                  delay: 0.1,
+                  ease: "easeInOut",
+                },
+              }}
+              className="w-full lg:w-[75%] text-justify text-[14px] md:text-[16px] lg:text-[20px] text-white font-poppins relative"
+            >
+              {description[currentIndex]}
+            </motion.div>
+          </AnimatePresence>
+        </motion.div>
+      </div>
+      <div className="w-full bg-[#FFFFFF] pb-[2.5%]">
+        <div className="w-full relative overflow-hidden mb-3">
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-center text-[#1F3576] font-poppins font-bold leading-snug relative z-[2]">
+            Meet <span className="text-[#FBD233]">Our</span> Team
+          </h2>
+          <h2 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[56px] text-center text-[#2F2F2F] font-poppins font-bold leading-snug absolute top-[2px] left-[2px] opacity-35 w-full">
+            Meet Our Team
+          </h2>
+        </div>
+        <div className="w-full h-auto px-[3.333%]">
+          <img src={"/assets/organogram.png"} alt={"organogram"} className="object-contain"/>
+        </div>
       </div>
     </>
   );
