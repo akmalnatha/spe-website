657d20e3e7e8df234fa004cedf87be1643e08200
akmalnatha <akmalabhirama2002@gmail.com>
2024-05-06 22:26:36 +0700

    feat:petromax

---
+++ src/lib/petromax.ts
@@ -0,0 +1,71 @@
+// Petromax.ts
+
+export interface PetromaxItem {
+    title: string;
+    image: string;
+    poster: string;
+    description: string;
+    link: string;
+  }
+  
+  const PetromaxData: PetromaxItem[] = [
+    {
+      title: "PETROMAX Vol.13",
+      image: "url('/assets/petromax_13.png')",
+      poster: "/assets/poster_13.png",
+      description: "We proudly brought up theme 'Humankind' as our main issue which covering: - An exclusive talks with Adam Alfares, Co-Founder of Menjadi Manusia - An insightful interview with Nurania Saubryani (ChE'13), a Facilities Surveillance Engineer at ExxonMobil - Remarkable opinions from the President of SPE ITS SC, SPE UGM SC, and SPE Undip SC - Oil price collapses to the lowest level for 18 years - Special tips during quarantine and many more.",
+      link: "https://issuu.com/speuisc/docs/output"
+    },
+    {
+      title: "PETROMAX Vol.14",
+      image: "url('/assets/petromax_14.png')",
+      poster: "/assets/poster_14.png",
+      description: "We proudly brought up 'Revolution' as our main theme featuring Mr. Mildo Nainggolan, Process Production Operation Manager at SKK Migas; Mr. Ismail Ibnu Haris Alhaj, Petroleum Engineer at Chevron; Ms. Melody Gita, Digital Business Development at Shell Lubricants; Ms. Astrini Yuswandari, Project Manager at Enerka; the President of SPE UNHAS SC, SPE UNPAD SC, and SPE PEM Akamigas SC, and many more.",
+      link: "https://issuu.com/speuisc/docs/petromax_vol._14_revolution"
+    },
+    {
+      title: "PETROMAX Vol.15",
+      image: "url('/assets/petromax_15.png')",
+      poster: "/assets/poster_15.png",
+      description: "Wholeheartedly presents ‘Into the Blue’ that feature many interviews from the Leader of Ocean Defender Indonesia, Co-Founder & Chief Growth Officer of CarbonEthics, Senior Drilling Engineer at Premier Oil, West Offshore Production Sr. Engineer at PT Pertamina Hulu Energi, and many more!",
+      link: "https://issuu.com/speuisc/docs/petromax_vol_15"
+    },
+    {
+      title: "PETROMAX Vol.16",
+      image: "url('/assets/petromax_16.png')",
+      poster: "/assets/poster_16.png",
+      description: "Gladly presents 'Energy & Aerospace' which features multiple interviews from the Director of Asia Pacific Energy Solutions, Researchers behind the very first bio-jet fuel flight test in Indonesia, Senior Aircraft Powerplant Engineer at GMF AeroAsia, Surface Treatment Development Manufacturing Engineer at PT Dirgantara, Aeromodelling UI, and many more!",
+      link: "https://issuu.com/speuisc/docs/petromax_vol._16"
+    },
+    {
+      title: "PETROMAX Vol.17",
+      image: "url('/assets/petromax_17.png')",
+      poster: "/assets/poster_17.png",
+      description: "Gladly presents 'Beyond Energy' which features multiple interviews from the Vice President Petrochemical Industry Business at PT Pertamina (Persero), President Director at PT. INEOS Aromatics Indonesia, millenials works in Pertamina Integrated Command Center at PT Pertamina, exclusive conversation with Lead Engineer at Qatar Petrochemical Company (QAPCO), and many more!",
+      link: "https://issuu.com/speuisc/docs/petromax_vol._17"
+    },
+    {
+      title: "PETROMAX Vol.18",
+      image: "url('/assets/petromax_18.png')",
+      poster: "/assets/poster_18.png",
+      description: "Proudly presents 'Automation and Digitalization in the Oil and Gas Industry' as our main theme which features multiple interviews from the Chief Technology Officer of Geoplat, millenials work at SLB, interesting talk with our alumnae, as well as president of SPE 2023, and many more!",
+      link: "https://issuu.com/speuisc/docs/peromax_18_fix_bgt"
+    },
+    {
+      title: "PETROMAX Vol.19",
+      image: "url('/assets/petromax_19.png')",
+      poster: "/assets/poster_19.png",
+      description: "Proudly presents 'Extreme Environments.' Immerse yourself in this magazine as we embark on a journey to uncover the challenges and triumphs of Earth's harshest landscapes. Our dedicated team brings you exclusive interviews with a Sr. Petrophysicist at Lukoil, the President of SPE International 2024, the SPE Java Chairperson, a millennial representative of Rainforest Alliance, and an onshore drilling engineer at Medco E&P, and many more!",
+      link: "https://bit.ly/PetromaxVolume19"
+    },
+    {
+      title: "PETROMAX Vol.20",
+      image: "url('/assets/petromax_20.png')",
+      poster: "/assets/poster_20.png",
+      description: "Proudly unveils 'Shaping Mankind,' an insightful exploration into the profound impact of the energy industry on human progress. Exclusive interviews feature leaders like the Executive Director of Reservoir Link Group, Sr. External Relations Advisor, and Project Manager of Clean Affordable and Secure Energy (CASE) at IESR, and many more!",
+      link: "https://bit.ly/PetromaxVolume20"
+    },
+  ];
+  
+  export default PetromaxData;
+  
