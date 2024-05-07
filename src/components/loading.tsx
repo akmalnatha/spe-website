84778ae4de9a37e7a89eeedc0d20dfde78c3fbbb
RakaAF123 <110679603+RakaAdmiharfan@users.noreply.github.com>
2024-05-07 20:00:27 +0700

    Update loading.tsx

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,17 +1,17 @@
 "use client";
 
+import React from "react";
 import ReactLoading from "react-loading";
 
-function LoadingPage({ isLoad }: { isLoad: boolean }) {
-  return (
-    <div
-      className={`${
-        isLoad ? "block" : "hidden"
-      } fixed z-[999] flex h-screen w-full items-center justify-center bg-white bg-opacity-95 backdrop-blur-md duration-300 ease-in-out`}
-    >
-      <ReactLoading type="cubes" color="#FCCF37" height={100} width={100} />
-    </div>
-  );
-}
+const LoadingSpinner = () => (
+  <div className="fixed z-[999] flex h-screen w-full items-center justify-center bg-white bg-opacity-95 backdrop-blur-md duration-300 ease-in-out">
+    <ReactLoading
+      type="spinningBubbles"
+      color="#FDD001"
+      height={100}
+      width={100}
+    />
+  </div>
+);
 
-export default LoadingPage;
+export default LoadingSpinner;
