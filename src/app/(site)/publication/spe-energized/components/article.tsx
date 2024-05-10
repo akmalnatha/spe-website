11fd5a7b04fb155c3e8e2206d212bad9d458632c
akmalnatha <akmalabhirama2002@gmail.com>
2024-05-10 19:54:30 +0700

    style: spe-energized, optimize article

--- src/app/(site)/publication/spe-energized/components/article.tsx
+++ src/app/(site)/publication/spe-energized/components/article.tsx
@@ -2,86 +2,79 @@ import { useEffect, useState } from "react";
 import articleData from "@/lib/articleData";
 import Link from "next/link";
 import AOS from "aos";
+import Button from "@/components/button";
 
 const Article = () => {
   useEffect(() => {
-    AOS.init({ duration: 1000 });
+    AOS.init({ duration: 1500 });
   });
 
   return (
     <>
       <div
-        className={`relative w-full h-full flex flex-col justify-center items-center`}
+        className={`w-full h-full flex flex-col px-[10%] lg:px-[4.0625%]`}
       >
         {articleData.map((elem) => (
-          <div key={elem.index}>
-            {/* Check if index is even */}
-            {elem.index % 2 !== 0 ? (
-              <>
-                {/* Render image on the left and text on the right */}
-                <div className="flex flex-col md:flex-row border-t-8 border-[#1F3576] py-11 mx-12 justify-center items-center gap-8">
-                  <img
-                    src={elem.image}
-                    alt={elem.alt}
-                    className="md:w-[300px] lg:w-[344px] h-auto object-cover"
-                    data-aos="fade-right"
-                  />
-                  <div
-                    className="md:w-1/2 flex flex-col gap-6 px-auto md:px-16"
-                    data-aos="fade-left"
-                  >
-                    <h1 className="text-[#142B6F] font-bold font-poppins sm: md:text-2xl lg:text-3xl">
-                      {elem.title}
-                    </h1>
-
-                    <p className="text-[#142B6F] text-justify font-light font-poppins sm:text-lg md:text-lg lg:text-2xl">
-                      {elem.text}
-                    </p>
-
-                    <Link
-                      href={elem.link}
-                      className="w-fit h-fit text-[#142B6F] font-bold font-poppins sm:text-lg md:text-lg lg:text-2xl items-end bg-[#FAD133] rounded-3xl px-6 py-2"
-                    >
-                      Read For More
-                    </Link>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <>
-                {/* Render image on the right and text on the left */}
-                <div className="flex flex-col md:flex-row border-t-8 border-[#1F3576] py-11 mx-12 justify-center items-center gap-8">
-                  <div
-                    className="md:w-1/2 flex flex-col gap-6 px-auto md:px-16"
-                    data-aos="fade-left"
-                  >
-                    <div className="flex md:justify-end">
-                      <h1 className="text-[#142B6F] font-bold font-poppins sm: md:text-2xl lg:text-3xl  ">
-                        {elem.title}
-                      </h1>
-                    </div>
-
-                    <p className="text-[#142B6F] font-light text-justify font-poppins sm:text-lg md:text-lg lg:text-2xl">
-                      {elem.text}
-                    </p>
-
-                    <Link
-                      href={elem.link}
-                      className="w-fit h-fit text-[#142B6F] font-bold font-poppins sm:text-lg md:text-lg lg:text-2xl justify-end items-end bg-[#FAD133] rounded-3xl px-6 py-2"
-                    >
-                      Read For More
-                    </Link>
-                  </div>
-                  <img
-                    src={elem.image}
-                    alt={elem.alt}
-                    className="sm:w-28 md:w-[300px] lg:w-[344px] h-auto object-cover"
-                    data-aos="fade-right"
+          <>
+            {/* Render image based on index */}
+            <div
+              key={elem.index}
+              className={`flex flex-col ${
+                elem.index % 2 !== 0 ? "md:flex-row" : "md:flex-row-reverse"
+              } border-t-8 border-[#1F3576] py-[12.5%] lg:py-[3.5%] px-[4%] justify-center items-center gap-[24px] lg:gap-[48px] xl:gap-[72px]`}
+            >
+              <img
+                src={elem.image}
+                alt={elem.alt}
+                className="md:w-[284px] lg:w-[344px] h-auto object-cover"
+                loading="lazy"
+                data-aos="fade-right"
+              />
+              <div
+                className={`md:grow flex flex-col gap-6 items-center ${
+                  elem.index % 2 !== 0 ? "md:items-start" : "md:items-end"
+                }`}
+                data-aos="fade-left"
+              >
+                <h1 className="text-[#142B6F] font-bold font-poppins text-[20px] md:text-[24px] lg:text-[28px] text-center lg:text-start">
+                  {elem.title}
+                </h1>
+                <p className={`text-[#142B6F] font-light text-justify font-poppins text-[16px] md:text-[20px] lg:text-[24px] ${
+                  elem.index % 2 !== 0 ? "md:text-justify" : "md:text-right"
+                }`}>
+                  {elem.text}
+                </p>
+                <div className="lg:block hidden">
+                  <Button
+                    type={"button"}
+                    textClass="text-[#1F3576] underline font-bold font-poppins"
+                    color="secondary"
+                    text="Read For More"
+                    shape="rounded-medium"
+                    fitContent
+                    onClick={() => window.open(
+                      elem.link,
+                      "_blank"
+                    )}
                   />
                 </div>
-              </>
-            )}
-          </div>
+                <div className="lg:hidden">
+                  <Button
+                    type={"button"}
+                    textClass="text-[#1F3576] underline font-bold font-poppins"
+                    color="secondary"
+                    text="Read For More"
+                    shape="rounded-small"
+                    fitContent
+                    onClick={() => window.open(
+                      elem.link,
+                      "_blank"
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
         ))}
       </div>
       <div className="w-full h-full"></div>
